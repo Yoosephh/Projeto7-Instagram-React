65aeeb6679cfc5548c574fdc34a7e0cae0434e0e
Yoosephh <jose.es.oleg@gmail.com>
2023-05-03 21:35:33 -0300

    Versão 1 com bug

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Body from './Body';
+import NavBar from './NavBar';
+
+function App() {
+  return(
+  <div>
+    <Body />
+    <NavBar />
+  </div>
+  )
+}
+export default App()
---
+++ src/components/Body.js
@@ -0,0 +1,18 @@
+import SideBar from './SideBar';
+import Stories from './Stories';
+import Posts from './Posts';
+
+export default function Body() {
+  return (
+    <div class='corpo'>
+      <div class='esquerda'>
+        <Stories />
+        <Posts />
+      </div>
+      <div class='direita'>
+        <SideBar />
+      </div>
+    </div>
+  )
+}
+
